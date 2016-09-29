95d79420fbb88d90f28ead808a8da65794eb1ffa
mrinspire <lee.white@email.cerrocoso.edu>
2016-09-29 06:22:07 -0700

    Project 3 final

--- footer-scripts/myft-scripts.js
+++ footer-scripts/myft-scripts.js
@@ -1,4 +1,14 @@
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myMainNav");
+    if (x.className === "mainNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "mainNav";
+    }
+}
+
 
 
 /* Start Auto Focus fallback for all browsers */
